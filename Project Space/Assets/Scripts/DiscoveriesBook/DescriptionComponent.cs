bb3a0ba086b74ecb5f5d9e265cada60ca11ac474
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2020-01-14 23:32:23 +0200

    Revert "Discovery book subsystem refactoring"
    This reverts commit 42b95ba18145ba29da4a5f00455fd4f74f1db5f6.

--- Project Space/Assets/Scripts/DiscoveriesBook/DescriptionComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-
-public class DescriptionComponent : MonoBehaviour
-{
-	public Text text;
-	public RectTransform rect;
-}
--- Project Space/Assets/Scripts/DiscoveriesBook/DiscoveryManager.cs
+++ Project Space/Assets/Scripts/DiscoveriesBook/DiscoveryManager.cs
@@ -8,134 +8,52 @@ using System.IO;
 
 public class DiscoveryManager : MonoBehaviour
 {
-    public DescriptionComponent discoveryDescriptionPrefab; // текст с описание выбранного открытия
-    public ButtonComponent discoveryPrefab; // кнопка-элемнт списка открытий
-    public ScrollRect discoveryScrollRect; // UI список открытий
-    public ScrollRect textArea; // UI описание открытия
 
-    private string fileName, lastName;
-    private List<Discovery> discoveries; // временное решение (для реализации одноступенчатой иерархии)
-    private List<Section> node; // !!! для многоуровневой иерархии (пока не используем)
-    private Section section;
-    private Discovery discovery;
-
-    private bool isFirstDiscovery = true;
+	private string _fileName, lastName;
+	private List<Discovery> node;
+	private Section section;
+	private Answer answer;
 
     public static DiscoveryManager Internal { get; set; }
 
-    void Awake()
-    {
-        Internal = this;
-    }
+	void Awake()
+	{
+		Internal = this;
+	}
 
-    public void DiscoveryBookOpen(string _fileName)
-    {
-        if (fileName == string.Empty)
-            return;
-        fileName = _fileName;
-        Debug.Log("Test0");
-        Load();
-    }
+	public void DiscoveryBookOpen(string fileName)
+	{
+		if (fileName == string.Empty) 
+			return;
+		_fileName = fileName;
+		Load();
+	}
 
-    void Load()
-    {
-        Debug.Log("Test1");
-        Debug.Log(lastName);
-        Debug.Log(fileName);
-        // нужно загрузить только названия открытий
-        // сейчас реализовано только 1этажная иерархия открытий
-        // то-есть слева список открытий - справа описание
-        // поэтому подгружаем весь список открытий и их описание в массивы
-        //if (fileName != null) // проверка, чтобы не загружать уже загруженный файл
-        //{
-            Debug.Log("Test2");
-            discoveries = new List<Discovery>();
+	void Load()
+	{
+		if (lastName == _fileName) // проверка, чтобы не загружать уже загруженный файл
+		{
+			//for (int i = 0; i < length; i++)
+			//{
+			//	SetSection(i);
+			//}
+			
+			return;
+		}
 
-            try // чтение элементов XML и загрузка значений атрибутов в массивы
-            {
-                TextAsset binary = Resources.Load<TextAsset>(fileName);
-                XmlTextReader reader = new XmlTextReader(new StringReader(binary.text));
 
-                int index = 0;
-                while (reader.Read())
-                {
-                    if (reader.IsStartElement("section"))
-                    {
-                        section = new Section();
-                        section.name = reader.GetAttribute("name");
-                        section.discoveries = new List<Discovery>();
-                        // !!! node.Add(section);
-
-                        XmlReader inner = reader.ReadSubtree();
-                        while (inner.ReadToFollowing("discovery"))
-                        {
-                            discovery = new Discovery();
-                            discovery.name = inner.GetAttribute("name");
-                            discovery.text = inner.GetAttribute("text");
-
-                            // !!! node[index].discoveries.Add(discovery);
-                            discoveries.Add(discovery); // временно 
-                            Debug.Log("Discovery added.");
-                        }
-                        inner.Close();
-
-                        index++;
-                    }
-                }
-
-                lastName = fileName;
-                reader.Close();
-            }
-            catch (System.Exception error)
-            {
-                Debug.Log(this + " Ошибка чтения файла книги открытий: " + fileName + ".xml >> Error: " + error.Message);
-                //scrollRect.gameObject.SetActive(false);
-                lastName = string.Empty;
-            }
-        //}
-
-        BuildBook();
-    }
-
-    void BuildElement(Discovery discovery)
-    {
-        ButtonComponent cloneObj = Instantiate(discoveryPrefab) as ButtonComponent;
-        DescriptionComponent desc = Instantiate(discoveryDescriptionPrefab) as DescriptionComponent;
-
-        cloneObj.text.text = discovery.name;
-        cloneObj.rect.SetParent(discoveryScrollRect.content);
-        
-        if(isFirstDiscovery)
-        {
-            Debug.Log(discovery.name);
-            Debug.Log("DESCRIPTION");
-            desc.text.text = discovery.text;
-            desc.rect.SetParent(textArea.content);
-            isFirstDiscovery = false;
-        }
-
-    }
-
-    void BuildBook()
-    {
-        foreach (Discovery discovery in discoveries)
-        {
-            BuildElement(discovery);
-        }
-    }
+	}
 }
-
-
 
 class Section
 {
-    public string name;
-    public List<Discovery> discoveries;
+	public string name;
+	public List<Discovery> discoveries;
 }
 
 
 class Discovery
 {
-    public string name;
-    public string text;
+	public string name;
+	public string text;
 }
